0d2d5ad6dd89bdeb2113a5c426a275ecc3b412bf
Aurora <aleeson@g.hmc.edu>
2017-10-28 01:09:04 -0700

    Code Compiles!
    Determines which point its on, whether or not its at the correct orientation, need to give directions to motors

--- client/include/tirtle/rpc.h
+++ client/include/tirtle/rpc.h
@@ -1,5 +1,1 @@
-#pragma once
-
-typedef uint8_t rpc_command;
-static const rpc_command SET_POSITION = 0;
-static const rpc_command LOAD_IMAGE = 1;
+/home/jbearer/Documents/tirtle/server/rpc.h
--- server/rpc.h
+++ server/rpc.h
@@ -1,1 +1,5 @@
-/home/jbearer/Documents/tirtle/client/include/tirtle/rpc.h
+#pragma once
+
+typedef uint8_t rpc_command;
+static const rpc_command SET_POSITION = 0;
+static const rpc_command LOAD_IMAGE = 1;
