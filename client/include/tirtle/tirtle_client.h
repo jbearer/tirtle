5980ccfc830724e40e5a322ed23fadfdfd05588a
Jeb Bearer <jbearer@hmc.edu>
2017-10-28 02:02:47 -0700

    Document interfaces

--- client/include/tirtle/tirtle_client.h
+++ client/include/tirtle/tirtle_client.h
@@ -12,9 +12,20 @@ namespace tirtle {
 
     struct tirtle_client
     {
+        /**
+         * Connect to a Tirtle server via Bluetooth on the given device name.
+         */
         tirtle_client(const std::string & dev);
 
+        /**
+         * Send a set of paths to the Tirtle. The Tirtle will immediately begin tracing out the
+         * given paths. This function must only be called once.
+         */
         void load_image(const std::vector<path_t> & paths);
+
+        /**
+         * Update the Tirtle with its current position and orientation.
+         */
         void set_position(point_t loc, angle_t angle);
 
         ~tirtle_client();
