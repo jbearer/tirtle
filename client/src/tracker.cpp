aaf57eeef927eaf2c83ef1e9ac4c4085733f56cb
Daniel Zhang <dzhang@hmc.edu>
2017-10-28 06:52:05 -0700

    started video capture

--- client/src/tracker.cpp
+++ client/src/tracker.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <ctime>
 #include <tuple>
+#include <memory>
 #include "math.h"
 #include "opencv2/opencv.hpp"
 #include "opencv2/aruco.hpp"
@@ -67,26 +68,34 @@ vector<cv::Point2f> getOuterCorners(vector<vector<cv::Point2f>> corners, vector<
 
 float getAngle(cv::Mat turtle_corners)
 {
-    cv::Point2f a = turtle_corners.at<cv::Point2f>(0);
-    cv::Point2f d = turtle_corners.at<cv::Point2f>(3);
+    cv::Point2f a = turtle_corners.at<cv::Point2f>(3);
+    cv::Point2f d = turtle_corners.at<cv::Point2f>(2);
+
     float delta_y = d.y - a.y;
     float delta_x = d.x - a.x;
     float radians = atan2(delta_y, delta_x);
     float degrees = radians / M_PI * 180.0;
-    return degrees + 180.0;
+
+    if (degrees < 0) {
+        return degrees + 360.0;
+    }
+    else {
+        return degrees;
+    }
 }
 
 tuple<cv::Point2f, float> findPoint(vector<cv::Point2f> outer_corners, vector<cv::Point2f> robot_corners)
 {
-    int width = 150;
-    int height = 200;
+
+    int width = 200;
+    int height = 150;
 
     cv::Point2f bottom_left = cv::Point2f(0, 0);
     cv::Point2f top_left = cv::Point2f(0, height);
-    cv::Point2f top_right = cv::Point2f(width, 0);
-    cv::Point2f bottom_right = cv::Point2f(width, height);
+    cv::Point2f top_right = cv::Point2f(width, height);
+    cv::Point2f bottom_right = cv::Point2f(width, 0);
 
-    vector<cv::Point2f> turtle_corners{bottom_left, top_left, top_right, bottom_right};
+    vector<cv::Point2f> turtle_corners{top_left, top_right, bottom_right, bottom_left};
     cv::Mat transform = cv::getPerspectiveTransform(outer_corners, turtle_corners);
 
     cv::Mat input{robot_corners};
@@ -99,41 +108,22 @@ tuple<cv::Point2f, float> findPoint(vector<cv::Point2f> outer_corners, vector<cv
     return make_tuple(output.at<cv::Point2f>(0), angle); // TODO: Make this the center
 }
 
+unique_ptr<tuple<cv::Point2f, float>> turtle_pos(cv::Mat& inputImage)
+{
+    auto corner_info = detectMarkers(inputImage);
+    auto corners = get<0>(corner_info);
+    auto ids = get<1>(corner_info);
 
+    if (corners.size() != 5){
+        return nullptr;
+    }
 
+    vector<cv::Point2f> outer_corners = getOuterCorners(corners, ids);
 
-// int main()
-// {
-//     cv::Mat inputImage = cv::imread("markers/test2.jpg");
-
-//     auto corner_info = detectMarkers(inputImage);
-//     auto corners = get<0>(corner_info);
-//     auto ids = get<1>(corner_info);
-
-//     vector<cv::Point2f> outer_corners = getOuterCorners(corners, ids);
-
-//     auto robot_corners = getCorners(0, ids, corners);
-
-//     auto point_angle = findPoint(outer_corners, robot_corners);
-//     cv::Point2f point = get<0>(point_angle);
-//     float angle = get<1>(point_angle);
-
-//     cout << "point is (" << point.x << ", " << point.y << "), angle is " << angle << endl;
-
-
-
-//     // for (auto id : markerIds) {
-//     //     cout << id << " ";
-//     // } cout << endl;
-
-//     for (auto pt : outer_corners) {
-//         cv::circle(inputImage, pt, 5, cv::Scalar(255, 0, 0), -1);
-//     }
-
-//     cv::imwrite("output.png", inputImage);
-
-//     cout << "hello" << endl;
-// }
+    auto point_angle = findPoint(outer_corners, robot_corners);
+    cv::imwrite("output.png", inputImage);
+    return make_unique<tuple<cv::Point2f, float>>(point_angle);
+}
 
 static void track(point_t & loc, angle_t & heading)
 {
