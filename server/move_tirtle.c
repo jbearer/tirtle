a5969716400c0ded5f49087cdb04bf38011082e2
Aurora Leeson <aleeson@g.hmc.edu>
2017-10-27 20:46:23 -0700

    starting to work

--- server/move_tirtle.c
+++ server/move_tirtle.c
@@ -1,2 +1,15 @@
 #include "path.h"
 #include "motor_control.h"
+
+static path_t *path;
+static 
+
+/* load image paths and store them for use */
+void load_image(path_t *path_, length_t num_paths);
+{
+  paths = path_;
+  
+}
+
+set_position(point_t loc, angle_t angle);
+
